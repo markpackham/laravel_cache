aa08c1d8a64cf35832552305d1d3bccc49814b21
mark <markpackham1@gmail.com>
2021-12-16 20:09:44 +0000

    use increment and decrement
    https://youtu.be/-NOOqIYEFwc?t=840

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -14,7 +14,7 @@ class PagesController extends Controller
         // Cache::add('cachekey2', 'Key num 2');
 
         // as you'd expect a forever method has no time limit
-        Cache::forever('cachekey2', "I am key 2");
+        Cache::forever('cachekey2', 4);
 
         // // the only way to get rid of the forever cache other than flushing everything
         // Cache::forget('cachekey2');
@@ -22,9 +22,14 @@ class PagesController extends Controller
         // // clear all cache
         // Cache::flush();
 
-        if (Cache::has('cachekey2')) {
-            dd('Cache does exist');
-        }
+        // if (Cache::has('cachekey2')) {
+        //     dd('Cache does exist');
+        // }
+
+        // will add 5 to a numerical value
+        Cache::increment('cachekey2', 5);
+        // will subtract 1 from a numerical value
+        Cache::decrement('cachekey2', 1);
 
         // $test = cache()->get();
         dd(Cache::get('cachekey2'));
