228ca292598785582401431ebeac62b023df722e
mark <markpackham1@gmail.com>
2021-12-16 14:01:45 +0000

    add cache that lasts 24 hours
    https://youtu.be/-NOOqIYEFwc?t=455

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PagesController extends Controller
 {
-    //
+    public function index()
+    {
+        Cache::put('cachekey', 'This should be a cache key', now()->addDay());
+
+        // $test = cache()->get();
+        dd(Cache::get('cachekey'));
+        return view('index');
+    }
 }
