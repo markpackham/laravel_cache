469c265ec0181c968ea1ca771e29b1679f32ac19
mark <markpackham1@gmail.com>
2021-12-16 17:50:22 +0000

    add, forever, flush and forget caches
    https://youtu.be/-NOOqIYEFwc?t=667

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -11,8 +11,19 @@ class PagesController extends Controller
     {
         Cache::put('cachekey', 'This should be a cache key', now()->addDay());
 
+        // Cache::add('cachekey2', 'Key num 2');
+
+        // as you'd expect a forever method has no time limit
+        Cache::forever('cachekey2', "I am key 2");
+
+        // the only way to get rid of the forever cache other than flushing everything
+        Cache::forget('cachekey2');
+
+        // clear all cache
+        Cache::flush();
+
         // $test = cache()->get();
-        dd(Cache::get('cachekey'));
+        dd(Cache::get('cachekey2'));
         return view('index');
     }
 }
