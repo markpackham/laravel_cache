26527b24a8b6961b49f9ebe6a0cabc0344df343e
mark <markpackham1@gmail.com>
2021-12-16 20:49:10 +0000

    setup event & listener
    https://youtu.be/-NOOqIYEFwc?t=1258

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Events\PostCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $dispatchesEvents = [
+        'created' => PostCreated::class
+    ];
 }
